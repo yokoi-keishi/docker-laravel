ae829bfdbe1be12b0ef34efc58f4c7c90093d9ec
yokoi-keishi <kc451.1213@gmail.com>
2020-07-23 10:16:52 +0900

    first commit

---
+++ backend/resources/views/top.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link rel="stylesheet" href="{{ asset('css/top.css') }}">
+
+  <title>Document</title>
+</head>
+<body>
+  <p>Hello!</p>
+</body>
+</html>
